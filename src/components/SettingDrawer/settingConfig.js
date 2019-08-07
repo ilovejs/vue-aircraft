6db882624a1dd1141712e37aa5cb01cedbe7f0a7
MZ <zhuangdeyouxiang@gmail.com>
2019-08-07 16:58:41 +1000

    translate some other module

--- src/components/SettingDrawer/settingConfig.js
+++ src/components/SettingDrawer/settingConfig.js
@@ -6,33 +6,33 @@ import themeColor from './themeColor.js'
 
 const colorList = [
   {
-    key: '薄暮', color: '#F5222D'
+    key: 'Dusk', color: '#F5222D'
   },
   {
-    key: '火山', color: '#FA541C'
+    key: 'Volcano', color: '#FA541C'
   },
   {
-    key: '日暮', color: '#FAAD14'
+    key: 'Sunset', color: '#FAAD14'
   },
   {
-    key: '明青', color: '#13C2C2'
+    key: 'Cyan', color: '#13C2C2'
   },
   {
-    key: '极光绿', color: '#52C41A'
+    key: 'Aurora green', color: '#52C41A'
   },
   {
-    key: '拂晓蓝（默认）', color: '#1890FF'
+    key: 'Dawn blue', color: '#1890FF'
   },
   {
-    key: '极客蓝', color: '#2F54EB'
+    key: 'Geek blue', color: '#2F54EB'
   },
   {
-    key: '酱紫', color: '#722ED1'
+    key: 'Purple', color: '#722ED1'
   }
 ]
 
 const updateTheme = newPrimaryColor => {
-  const hideMessage = message.loading('正在切换主题！', 0)
+  const hideMessage = message.loading('Switching theme now!', 0)
   themeColor.changeColor(newPrimaryColor).finally(t => {
     hideMessage()
   })
