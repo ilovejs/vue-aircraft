31aa0327958ebbf251528e090705007ee38da5c3
ilovejs <zhuangdeyouxiang@gmail.com>
2019-08-13 15:39:46 +1000

    eslint ignore style autofix

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import '@babel/polyfill'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store/'
+import store from './store'
 import { VueAxios } from './utils/request'
 
-// mock is exported under index.js
+// TODO: mock is exported under index.js
 // loading services according to node settings
 // Turn off will cause /api/2step not available in login
 // Check console for loading logs
@@ -17,7 +17,8 @@ import bootstrap from './core/bootstrap'
 import './core/use'
 import './permission' // permission control
 import './utils/filter' // global filter
-// ??
+
+// disable warning "You are running Vue in development mode."
 Vue.config.productionTip = true
 
 // mount axios Vue.$http and this.$http
@@ -26,8 +27,8 @@ Vue.use(VueAxios)
 new Vue({
   router,
   store,
-  created () {
+  created() {
     bootstrap()
   },
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
--- src/mock/services/auth.js
+++ src/mock/services/auth.js
@@ -8,41 +8,35 @@ const login = (options) => {
   const body = getBody(options)
   console.log('mock: body', body)
   if (!username.includes(body.username) || !password.includes(body.password)) {
-    return builder({ isLogin: true }, '账户或密码错误', 401)
+    return builder({ isLogin: true }, 'Account or password issue', 401)
   }
 
   return builder({
-    'id': Mock.mock('@guid'),
-    'name': Mock.mock('@name'),
-    'username': 'admin',
-    'password': '',
-    'avatar': 'https://gw.alipayobjects.com/zos/rmsportal/jZUIxmJycoymBprLOUbT.png',
-    'status': 1,
-    'telephone': '',
-    'lastLoginIp': '27.154.74.117',
-    'lastLoginTime': 1534837621348,
-    'creatorId': 'admin',
-    'createTime': 1497160610259,
-    'deleted': 0,
-    'roleId': 'admin',
-    'lang': 'zh-CN',
-    'token': '4291d7da9005377ec9aec4a71ea837f'
+    id: Mock.mock('@guid'),
+    name: Mock.mock('@name'),
+    username: 'admin',
+    password: '',
+    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/jZUIxmJycoymBprLOUbT.png',
+    status: 1,
+    telephone: '',
+    lastLoginIp: '27.154.74.117',
+    lastLoginTime: 1534837621348,
+    creatorId: 'admin',
+    createTime: 1497160610259,
+    deleted: 0,
+    roleId: 'admin',
+    lang: 'zh-CN',
+    token: '4291d7da9005377ec9aec4a71ea837f',
   }, '', 200, { 'Custom-Header': Mock.mock('@guid') })
 }
 
-const logout = () => {
-  return builder({}, '[测试接口] 注销成功')
-}
+const logout = () => builder({}, '[测试接口] 注销成功')
 
-const smsCaptcha = () => {
-  return builder({ captcha: Mock.mock('@integer(10000, 99999)') })
-}
+const smsCaptcha = () => builder({ captcha: Mock.mock('@integer(10000, 99999)') })
 
-const twofactor = () => {
-  return builder({ stepCode: Mock.mock('@integer(0, 1)') })
-}
+const twofactor = () => builder({ stepCode: Mock.mock('@integer(0, 1)') })
 
-//expose as api
+// expose as api
 Mock.mock(/\/auth\/login/, 'post', login)
 Mock.mock(/\/auth\/logout/, 'post', logout)
 Mock.mock(/\/account\/sms/, 'post', smsCaptcha)
